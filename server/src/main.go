7cfccd69956de6adc201c2e03973aed2edb9938e
momakas <kkgtycb@yahoo.co.jp>
2024-07-30 00:13:28 +0900

    simple chat 1.

--- server/src/main.go
+++ server/src/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"websocket/src/domain"
 	"websocket/src/handlers"
-	"websocket/src/services"
 
 	// db接続
 	"database/sql"
@@ -24,19 +23,20 @@ func setupDB(dbDriver string, dsn string) (*sql.DB, error) {
 
 func main() {
 	// db接続
-	dbDriver = "postgres"
-	dsn := "host=127.0.0.1 port=5432 user=user password=password dbname=dbname sslmode=disable"
-	db, err := setupDB(dbDriver, dsn)
-	defer db.Close()
+	// dbDriver = "postgres"
+	// dsn := "host=127.0.0.1 port=5432 user=user password=password dbname=dbname sslmode=disable"
+	// db, err := setupDB(dbDriver, dsn)
+	// defer db.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// チャット
-	pubsub := services.NewPubSubService()
-	hub := domain.NewHub(pubsub)
-	go hub.SubscribeMessages()
+	// pubsub := services.NewPubSubService()
+	hub := domain.NewHub()
+	// hub := domain.NewHub(pubsub)
+	// go hub.SubscribeMessages()
 	go hub.RunLoop()
 
 	http.HandleFunc("/ws", handlers.NewWebsocketHandler(hub).Handle)
